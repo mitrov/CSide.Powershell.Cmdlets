f019104ad18d8a034326addde1440bc37a1ca37f
Jan Hoek <github@uncommonsense.nl>
2015-11-19 21:50:57 +0100

    Add Pester tests

---
+++ New-NAVObjectFilter.Tests.ps1
@@ -0,0 +1,13 @@
+﻿Describe "New-NAVObjectFilter" {
+    It "Returns an empty string if no parameters are specified" {
+        New-NAVObjectFilter | Should Be ''
+    }
+
+    It "Returns a Type filter if some object types are specified" {
+        New-NAVObjectFilter -Table -Page -Codeunit | Should Be 'Type=Table|Page|Codeunit'
+    }
+
+    It "Returns an empty Type filter is all object types are specified" {
+        New-NAVObjectFilter -Table -Page -Codeunit -Report -MenuSuite -XmlPort -Query | Should Be ''
+    }
+}
