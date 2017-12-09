2a82255c6791c23e9038887931d07d22f5299789
Jan Hoek <github@uncommonsense.nl>
2017-12-09 05:44:34 +0100

    Improved Update-README.ps1 script

--- Update-README.ps1
+++ Update-README.ps1
@@ -1,14 +1,11 @@
-﻿. C:\users\jhoek\GitHub\Scripts\Get-HelpAsMarkDown.ps1
-Set-Location C:\Users\jhoek\Documents\WindowsPowerShell\Modules\UncommonSense.Nav.Automation
+﻿Import-Module UncommonSense.Nav.Automation -Force -Verbose
 
-Import-Module UncommonSense.Nav.Automation -Force -Verbose
 Get-Command `
     -Module UncommonSense.Nav.Automation | `
     Sort-Object -Property Noun,Verb | `
         Get-HelpAsMarkDown `
             -Title UncommonSense.Nav.Automation `
             -Description 'PowerShell utils for Microsoft Dynamics NAV' `
-            -PrefacePath (Join-Path $Pwd PREFACE.md) `
-            -PostfacePath (JOin-Path $Pwd POSTFACE.md) |`
-                Out-File .\README.md #>
-
+            -PrefacePath (Join-Path -Path $PSScriptRoot -ChildPath PREFACE.md) `
+            -PostfacePath (Join-Path -Path $PSScriptRoot -ChildPath POSTFACE.md) |`
+                Out-File -FilePath (Join-Path -Path $PSScriptRoot -ChildPath README.md) -Encoding utf8
